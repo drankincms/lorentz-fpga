3f3dc261db7952121ea7da9f15604cdc83c144d9
Dylan R <dylansr90@gmail.com>
2023-11-29 08:19:30 -0600

    adding tcl

---
+++ project.tcl
@@ -0,0 +1,10 @@
+variable project_name
+set project_name "myproject"
+variable backend
+set backend "vivado"
+variable part
+set part "xcvu13p-flga2577-1-e"
+variable clock_period
+set clock_period 5
+variable clock_uncertainty
+set clock_uncertainty 12.5%
